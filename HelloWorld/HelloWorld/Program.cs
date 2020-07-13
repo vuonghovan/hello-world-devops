bbb4c2e513c207e7daa0d06fd92c844e5ef09b96
Ho Van Vuong <vuonghovan@gmail.com>
2020-07-14 00:21:03 +0700

    add port for deploy

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -20,7 +21,13 @@ namespace HelloWorld
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+
+                        serverOptions.Listen(IPAddress.Loopback, 6000);
+
+                    })
+                    .UseStartup<Startup>();
                 });
     }
 }
