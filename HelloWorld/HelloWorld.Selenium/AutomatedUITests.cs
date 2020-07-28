b68f31289ecdb4b0be3959c66e0d418dc394b101
Ho Van Vuong <vuonghovan@gmail.com>
2020-07-28 15:16:39 +0700

    fix selenium project

--- HelloWorld/HelloWorld.Selenium/AutomatedUITests.cs
+++ HelloWorld/HelloWorld.Selenium/AutomatedUITests.cs
@@ -1,14 +1,21 @@
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HelloWorld.Selenium
 {
-    [TestClass]
+    [TestFixture]
     public class AutomatedUITests : IDisposable
     {
         private IWebDriver _driver;
+        private string _url = "https://qa-hello-world.azurewebsites.net/home/create";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+        }
 
         public void Dispose()
         {
@@ -16,21 +23,19 @@ namespace HelloWorld.Selenium
             _driver.Dispose();
         }
 
-        [TestMethod]
+        [Test]
         public void Create_WhenExecuted_Return_RedirectToIndex()
         {
-            // Init
             string strTestInValid = "Azure DevOps Sharing!";
             string strTestValid = "Azure DevOps!";
+            _driver.Navigate().GoToUrl(_url);
 
-            _driver = new ChromeDriver();
-            _driver.Navigate().GoToUrl("https://qa-hello-world.azurewebsites.net/home/create");
-
-            //Action
+            //Act
             _driver.FindElement(By.Id("Name")).SendKeys(strTestValid);
             _driver.FindElement(By.Id("btnCreate")).Click();
 
             var element = _driver.FindElement(By.Id("Name"));
+            //Assert
             Assert.AreEqual(strTestValid, element.GetAttribute("value"));
         }
     }
